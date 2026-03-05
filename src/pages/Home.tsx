e1874b923bb012ed62000e58618dd4efdc93d78d
Alter25 <alter@LozPc.localdomain>
2026-03-05 00:20:31 -0600

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,13 @@
 export default function Home (){
   return (
     <section className="w-full h-screen flex flex-col items-center">
-      <div className="flex flex-col items-start md:px-12 md:py-12 lg:px-12 w-full border border-red-400 bg-purple-500">
-        <section className="flex flex-col w-40">
-          <h1 className="text-2xl m-auto my-12 sm:text-3xl lg:text-5xl font-semibold">Bienvenido a tu tienda de confianza.</h1>
-          <h2 className="text-5xl font-semibold text-emerald-300">Emma</h2>
+      <div className="flex not-sm:flex-col justify-between px-2 md:px-6 md:py-12 lg:px-12 w-full pt-12 bg-purple-500">
+        <section className="flex flex-col not-sm:h-70 w-60 lg:w-120">
+          <h1 className="text-5xl font-semibold text-white font-serif leading-16">Tu tienda de confianza.</h1>
         </section>
-
-        <img src="canasta.png" alt="canasta de abarrotes" />
+        <section className="">
+          <img src="canasta.png" alt="canasta de abarrotes" className="md:w-120 lg:w-160" />
+        </section>
       </div>
     </section>
   );
