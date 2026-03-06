16f77ddcc08897690ed329285c98937d5fd5013d
Alter25 <alter@LozPc.localdomain>
2026-03-06 14:28:55 -0600

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 export const Header = () => {
   return (
     <header className="fixed px-2 sm:px-3 flex justify-center w-full">
-      <div className="border border-red-400 w-screen">
+      <div className=" w-screen">
         <div className="flex justify-between">
           {/* logo */}
           <div className="w-fit px-1 h-10 bg-linear-to-br from-blue-500 to-purple-600 rounded-lg flex items-center justify-center">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,17 @@
 
 
-export default function Home (){
-  return (
-    <section className="w-full h-screen flex flex-col items-center">
-      <div className="flex not-sm:flex-col justify-between px-2 md:px-6 md:py-12 lg:px-12 w-full pt-12 bg-purple-500">
-        <section className="flex flex-col not-sm:h-70 w-60 lg:w-120">
-          <h1 className="text-5xl font-semibold text-white font-serif leading-16">Tu tienda de confianza.</h1>
-        </section>
-        <section className="">
-          <img src="canasta.png" alt="canasta de abarrotes" className="md:w-120 lg:w-160" />
-        </section>
-      </div>
-    </section>
-  );
+export default function Home() {
+	return (
+		<section className="w-full h-screen bg-olive-300">
+			<div className="not-sm:flex-col flex justify-start py-12 bg-purple-500">
+				<section className="flex flex-col not-sm:h-60 not-sm:px-6 md:px-12 items-center justify-center md:w-60 lg:w-120">
+					<h1 className="w-fit text-3xl md:text-4xl lg:text-7xl not-sm:mx-auto font-semibold text-white font-serif leading-8 lg:leading-12 mb-4">Tu tienda de confianza.</h1>
+					<h2 className="w-fit text-2xl md:text-3xl lg:text-4xl text-black/60 font-bold leading-1 md:leading-8 lg:leading-12"> Donde obtienes informacion de calidad.<span className="text-5xl">   😂</span></h2>
+				</section>
+				<section className="w-full">
+					<img src="canasta.png" alt="canasta de abarrotes" className="m-auto not-sm:w-80 md:w-120 lg:w-140" />
+				</section>
+			</div>
+		</section>
+	);
 };
