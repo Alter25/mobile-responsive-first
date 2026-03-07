24a143cf2f8940e5e9fd0876faab88293768e76a
Alter25 <alter@LozPc.localdomain>
2026-03-07 00:41:03 -0600

    update

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function Marquee({ elementos }: Props) {
   return <article className="w-full overflow-hidden">
-    <section className="w-[100vw] flex justify-between">
+    <section className="">
       {
         elementos.map((item, index) => {
           return <Card key={index} name={item.name} imageUrl={item.imageUrl} />
