04a45d56f1637c1c56dc786ce53fcc56ba19ccc2
Alter25 <alter@LozPc.localdomain>
2026-03-11 00:21:51 -0600

    bug:footer position fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import { Mensajes } from "@/components/data/carrusel";
 
 export default function Home() {
 	return (
-		<section className="w-full h-screen">
+		<section className="w-full">
 			<div className="not-sm:flex-col flex justify-start pt-16 pb-4 md:justify-evenly bg-purple-500">
 				<section className="ml-1 flex flex-col not-sm:h-60 not-sm:px-6 items-center justify-center md:w-180 lg:w-260 border">
 					<h1 className="w-fit text-4xl md:text-5xl lg:text-6xl font-semibold text-white font-serif leading-10 sm:leading-14 lg:leading-16 mb-4 lg:-translate-x-8">Tu tienda de confianza.</h1>
