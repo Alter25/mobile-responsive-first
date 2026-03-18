16b631a9df39d1fa25ab288e22d81b91ce33b32e
Alter25 <alter@LozPc.localdomain>
2026-03-18 17:29:23 -0600

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,16 +4,14 @@ import { Button } from "./ui/button";
 
 export const Header = () => {
   return (
-    <header className=" mx-auto border border-slate-200 rounded-2xl px-6 py-3 sm:px-3 flex justify-center w-full bg-white z-10">
-      <div className=" w-screen">
-        <div className="w-screen flex justify-center items-center">
-          <div className="w-fit px-1 ml-4 h-10 bg-linear-to-br rounded-lg flex items-center justify-center bg-indigo-400">
+    <header className=" mx-auto  rounded-2xl px-2 py-3 sm:px-2 flex w-full bg-white z-10 max-w-screen-2xl">
+      <div className="w-full">
+        <div className="w-full px-2 flex justify-between items-center">
+          <div className="w-fit px-1 h-10 bg-linear-to-br rounded-lg flex items-center justify-center bg-indigo-400">
             <span className="text-primary font-medium text-xl">Emma</span>
           </div>
-          <div className="mr-4">
-            <NavbarButton />
-          </div>
-          <div className="hidden min-w-140 sm:justify-between sm:flex gap-8 mr-8 sm:mx-auto">
+          <NavbarButton />
+          <div className="hidden sm:justify-between sm:flex gap-8">
             <nav className="flex justify-between">
               <ol className="flex items-center justify-around gap-8 w-full">
                 <li><Link to="/">Inicio</Link></li>
@@ -22,7 +20,9 @@ export const Header = () => {
                 <li><Link to="/Contact">Contacto</Link></li>
               </ol>
             </nav>
-            <div className="hidden items-end md:flex"><Button><Link to="/Login">Login</Link></Button></div>
+          </div>
+          <div className="hidden items-end md:flex">
+            <Button><Link to="/Login">Login</Link></Button>
           </div>
         </div>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,28 +7,49 @@ import { Mensajes } from "@/components/data/carrusel";
 export default function Home() {
 	return (
 		<section className="w-full">
-			<div className="not-sm:flex-col flex justify-start pt-16 pb-4 md:justify-evenly bg-hero-bg rounded-b-xl">
-				<section className="ml-2 flex flex-col not-sm:h-60 not-sm:px-6 items-center justify-center md:w-180 lg:w-260">
-					<h1 className="w-fit text-4xl md:text-5xl lg:text-6xl font-semibold text-brand-primary font-serif leading-10 sm:leading-14 lg:leading-16 mb-4 lg:-translate-x-8 xl:-translate-x-4">Tu tienda de confianza.</h1>
+			<div className="flex flex-col sm:flex-row justify-start pt-16 pb-4 sm:justify-evenly bg-hero-bg rounded-b-xl">
+
+				<section className="flex flex-col px-6 items-center justify-center sm:items-start sm:w-180 lg:w-260">
+					<h1 className="
+            w-fit 
+            text-4xl sm:text-5xl lg:text-6xl 
+            font-semibold font-serif 
+            text-brand-primary 
+            leading-10 sm:leading-14 lg:leading-16 
+            mb-4
+          ">
+						Tu tienda de confianza.
+					</h1>
 					<div className="flex flex-col justify-start gap-6">
-						<h2 className="w-fit text-2xl md:text-3xl lg:text-4xl text-black/60 font-bold leading-8 md:leading-9 lg:leading-12"> Donde obtienes informacion de calidad.</h2>
+						<h2 className="
+              w-fit 
+              text-2xl sm:text-3xl lg:text-4xl 
+              text-black/60 font-bold 
+              leading-8 sm:leading-9 lg:leading-12
+            ">
+							Donde obtienes informacion de calidad.
+						</h2>
 						<h3 className="text-5xl">😂</h3>
 					</div>
 				</section>
-				<section className="lg:ml-24">
-					<img src="canasta.png" alt="canasta de abarrotes" className="m-auto w-80 -translate-x-2 sm:w-100 md:w-100 lg:w-140 xl:w-130" />
+
+				<section className="flex items-center justify-center lg:ml-24">
+					<img
+						src="canasta.png"
+						alt="canasta de abarrotes"
+						className="w-80 sm:w-100 lg:w-140 xl:w-130"
+					/>
 				</section>
+
 			</div>
 			<div className="w-full">
 				<Marquee elementos={Productos} />
 			</div>
 			<div className="w-full flex flex-col md:flex-row py-8 items-center sm:justify-center flex-wrap">
-				{
-					Mensajes.map((item, index) => (
-						<WrapperCard key={index} title={item.content.header} text={item.content.text} name={item.name} />
-					))
-				}
+				{Mensajes.map((item, index) => (
+					<WrapperCard key={index} title={item.content.header} text={item.content.text} name={item.name} />
+				))}
 			</div>
 		</section>
 	);
-};
+}
