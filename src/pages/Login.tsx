b6009071e7807891bfd48823f46c3a35b9f41ea4
Alter25 <alter@LozPc.localdomain>
2026-03-23 19:30:21 -0600

    update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,8 +11,8 @@ import { Input } from "@/components/ui/input";
 
 
 export default function Login() {
-  return <section className="w-full p-4 flex justify-center items-center">
-    <FieldSet className="bg-indigo-400 rounded-md w-80 h-90">
+  return <section className="w-full p-4 flex justify-center items-center mt-4 sm:mt-12">
+    <FieldSet className="bg-indigo-400 rounded-md w-80 h-100">
       <FieldGroup className="my-8">
         <Field className="px-12">
           <FieldLabel htmlFor="username">
@@ -26,12 +26,11 @@ export default function Login() {
           </FieldLabel>
           <Input id="password" type="password" />
           <FieldDescription>8 Cararacteres como minimo!</FieldDescription>
-          <div className="flex gap-2 items-center">
+          <div className="flex gap-2 items-center mt-8">
             <Checkbox id="keepsigned" />
-            <FieldLabel>Mantener iniciada la sesion</FieldLabel>
+            <FieldLabel className="text-black">Mantener iniciada la sesion</FieldLabel>
           </div>
-
-          <Button className="my-8">Ingresar</Button>
+          <Button className="mt-4">Ingresar</Button>
         </Field>
       </FieldGroup>
     </FieldSet>
