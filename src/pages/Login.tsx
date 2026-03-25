56cc6f385cd247607241c00390eb2f2cc4a1f9ca
Alter25 <alter@LozPc.localdomain>
2026-03-24 20:02:47 -0600

    update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,9 +8,18 @@ import {
   FieldDescription
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import React, { useState } from "react";
+import { verifyPassword } from "@/lib/utils";
 
 
 export default function Login() {
+  const [password, setPassword] = useState<string>("");
+
+  const handlePassword = (s: React.ChangeEvent<HTMLInputElement>) => {
+    const p = s.target.value;
+    setPassword(p);
+  }
+
   return <section className="w-full p-4 flex justify-center items-center mt-4 sm:mt-12">
     <FieldSet className="bg-indigo-400 rounded-md w-80 h-100">
       <FieldGroup className="my-8">
@@ -24,8 +33,9 @@ export default function Login() {
           <FieldLabel htmlFor="password">
             Password
           </FieldLabel>
-          <Input id="password" type="password" />
+          <Input id="password" type="text" value={password} onChange={handlePassword} />
           <FieldDescription className="text-purple-700">8 Cararacteres como minimo!</FieldDescription>
+          {verifyPassword(password) && <p>ok</p>}
           <div className="flex gap-2 items-center mt-8">
             <Checkbox id="keepsigned" />
             <FieldLabel htmlFor="keepsigned" className="text-black">Mantener iniciada la sesion</FieldLabel>
